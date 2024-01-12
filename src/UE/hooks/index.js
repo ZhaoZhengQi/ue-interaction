a36b579d8a29ced8e94ca68598db56b99ae31703
zzq <zhaoochenn@163.com>
2024-01-12 09:20:07 +0800

    新增配置项

--- src/UE/hooks/index.js
+++ src/UE/hooks/index.js
@@ -4,15 +4,20 @@
  * @param {object} data 事件数据
  * @param {object} options 选项
  */
-const ueSend = (eventName, data, options = { isJson: true }) => {
-  if (options.isJson) {
+const ueSend = (eventName, data, options = { isJson: true, isLog: false }) => {
+  // 是否转换为json并且data不是json
+  if (options.isJson && typeof data === "object") {
     data = JSON.stringify(data);
+  }
+  // 是否打印日志
+  if (options.isLog) {
+    console.log("通知ue->事件名称：", eventName, "参数：" + data);
   }
   ue4(eventName, data);
 };
 
 /**
- * UE接收事件
+ * 接收UE事件
  * @param {string} eventName 事件名称
  * @param {function} callback 回调函数
  */
